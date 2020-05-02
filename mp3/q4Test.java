30ca3d7186b975f36c0e6e295ca0b445cc2ffbe4
coleamoore <60358781+coleamoore@users.noreply.github.com>
2020-05-01 22:08:30 -0400

    test file for q4

---
+++ mp3/q4Test.java
@@ -0,0 +1,31 @@
+package jenkins.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class q4Test {
+
+	@Test
+	public void testTilde() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		String filename = "test~this";
+		assertEquals(test.idFromFilename(filename), "testThis");
+	}
+
+	@Test
+	public void testDollarSignAndExclamation() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		String filename = "test$0021";
+		assertEquals(test.idFromFilename(filename), "test!");
+	}
+
+	@Test
+	public void testDollarSign() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		String filename = "test$0055";
+		assertEquals(test.idFromFilename(filename), "testU");
+	}
+
+
+}
